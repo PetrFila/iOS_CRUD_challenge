e884d361e8f76ef0a15160ea90730d51de4a7d66
Petr Fila <petrfila@192-168-1-4.tpgi.com.au>
2019-05-09 22:31:17 +1000

    initial commit. Created basic 2 screen app using navigationController

---
+++ stickers_CRUD/DetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsViewController.swift
+//  stickers_CRUD
+//
+//  Created by Petr Fila on 9/5/19.
+//  Copyright © 2019 Petr Fila. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blue
+    }
+    
+    
+}
---
+++ stickers_CRUD/NewItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewItemViewController.swift
+//  stickers_CRUD
+//
+//  Created by Petr Fila on 9/5/19.
+//  Copyright © 2019 Petr Fila. All rights reserved.
+//
+
+import UIKit
+
+class AddNewItemViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.gray
+    }
+}
