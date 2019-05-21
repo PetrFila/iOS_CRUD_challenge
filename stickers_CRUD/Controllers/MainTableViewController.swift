70f66bf71566429cec492a96dfc0b32809bd6a53
Petr Fila <petrfila@192-168-1-4.tpgi.com.au>
2019-05-21 23:38:59 +1000

    data gets pulled and displayed

--- stickers_CRUD/Controllers/MainTableViewController.swift
+++ stickers_CRUD/Controllers/MainTableViewController.swift
@@ -56,9 +56,6 @@ class TableViewController: UITableViewController {
         }
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section
@@ -70,24 +67,12 @@ class TableViewController: UITableViewController {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? MainTableViewCell
             else { return UITableViewCell() }
-////        let accessData = dataInDictionary["stickers"]!
-//        let dataToRow = accessData[indexPath.row]
-//        cell.title = dataToRow.title
+
+        let accessData = dataInDictionary["stickers"]
+        let dataToRow = accessData?[indexPath.row]
+        cell.title = dataToRow?.title
         
         return cell
-    }
-    
-    // swipe menu
-    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let deleteAction = UIContextualAction(style: UIContextualAction.Style.destructive, title: "Delete"){
-            (action, view, completion) in
-            self.showActionSheet()
-
-//            self.tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
-            completion(false)
-        }
-        
-        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -99,15 +84,18 @@ class TableViewController: UITableViewController {
         self.navigationController?.pushViewController(detailScreen, animated: true)
 
     }
-
     
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-  
-    @objc func moveToAddNewItemScreen() {
-        show(AddNewItemViewController(), sender: self)
+    // MARK swipe menu
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: UIContextualAction.Style.destructive, title: "Delete"){
+            (action, view, completion) in
+            self.showActionSheet()
+            
+            //            self.tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+            completion(false)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
     
     func showActionSheet() {
@@ -124,9 +112,17 @@ class TableViewController: UITableViewController {
         
         // present the action sheet on display
         self.present(actionSheet, animated: true, completion: nil)
-
+        
     }
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @objc func moveToAddNewItemScreen() {
+        show(AddNewItemViewController(), sender: self)
+    }
     
 }
 
+//EABC..BAB...GG....DEF...EDE....CB......low A....
--- stickers_CRUD/Views/MainTableViewCell.swift
+++ stickers_CRUD/Views/MainTableViewCell.swift
@@ -11,13 +11,91 @@ import UIKit
 class MainTableViewCell: UITableViewCell {
     
     var title: String?
-//    var description: String?
+    var desc: String?
     var rating: Int?
     var url: URL?
     
+    lazy var titleName: UILabel = {
+        var text = UILabel()
+//        text.lineBreakMode = .byWordWrapping
+//        text.numberOfLines = 0
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    
+    lazy var descrip: UILabel = {
+        var textView = UILabel()
+        textView.isUserInteractionEnabled = false
+        textView.numberOfLines = 0
+        textView.lineBreakMode = .byWordWrapping
+        return textView
+    }()
+    
+    lazy var ratingNumber: UILabel = {
+        var textView = UILabel()
+        return textView
+    }()
+    
+    lazy var urlImage: UILabel = {
+        var image = UILabel()
+        return image
+    }()
+
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: UITableViewCell.CellStyle.default, reuseIdentifier: reuseIdentifier)
+        
+        self.addSubview(titleName)
+        self.addSubview(descrip)
+        self.addSubview(ratingNumber)
+        self.addSubview(urlImage)
+        
+        setConstraints()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if let title = title {
+            titleName.text = title
+        }
+        
+//        titleName.text = "TEST"
+
+        
+        if let desc = desc {
+            descrip.text = desc
+        }
+        
+        
+        if let rating = rating {
+            ratingNumber.text = String(rating)
+        }
+        
+//        if let url = url {
+//            urlImage.text = url
+//        }
+    }
+    
+    
+    func setConstraints() {
+       
+//        titleName.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        titleName.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        
+//        NSLayoutConstraint.activate([
+//            titleName.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+//            titleName.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+
+//            titleName.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+//            titleName.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10)
+//            titleName.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+//            titleName.heightAnchor.constraint(equalToConstant: 40)
+//            ])
+        
+        
+        
+      
     }
     
     required init?(coder aDecoder: NSCoder) {
